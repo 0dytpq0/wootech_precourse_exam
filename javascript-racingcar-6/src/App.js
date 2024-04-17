4eb95658d464fd471f2241e9933edf872295ee9d
Joseph Park <rkfnahs12e@gmail.com>
2024-04-17 20:34:19 +0900

    test

---
+++ javascript-racingcar-6/src/App.js
@@ -0,0 +1,12 @@
+import Controller from './Controller.js';
+import CarRacing from './model/CarRacing.js';
+import Winner from './model/Winner.js';
+import Validator from './model/validator.js';
+import { inputView } from './view/Input.js';
+import { OutputView } from './view/Output.js';
+
+class App {
+  async play() {}
+}
+
+export default App;
